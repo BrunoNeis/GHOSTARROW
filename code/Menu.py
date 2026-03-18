4fa69025d9db62aa97ebb4f8259f50c0ab8f788d
BRUNONEIS <brunobneis@gmail.com>
2026-03-17 22:24:04 -0300

    Creat UML Connecting Menu and Game classes 1

--- code/Menu.py
+++ code/Menu.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from pydoc_data.topics import topics
+import pygame
+
 
 class Menu:
     def __init__(self, window):
         self.window = window
-
+        self.surf = pygame.image.load("./asset/Menubg.png")
+        self.rect = self.surf.get_rect(left = 0, top=0 )
 
     def run(self, ):
-
+        self.window.blit(source=self.surf, dest=self.rect)
+        pygame.display.flip()
+        pass
 
 
 
