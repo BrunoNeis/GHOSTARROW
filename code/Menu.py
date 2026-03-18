da978bdc9e03eb29dc06c29c824ae2090432ba49
BRUNONEIS <brunobneis@gmail.com>
2026-03-17 22:54:51 -0300

    Creat MENU musica

--- code/Menu.py
+++ code/Menu.py
@@ -10,11 +10,17 @@ class Menu:
         self.surf = pygame.image.load("./asset/Menubg.png")
         self.rect = self.surf.get_rect(left = 0, top=0 )
 
-    def run(self, ):
+    def run(self,):
+        pygame.mixer_music.load("./asset/grim.mp3")
+
         self.window.blit(source=self.surf, dest=self.rect)
         pygame.display.flip()
         pass
 
+        for event in pygame.event.get():
+         if event.type == pygame.QUIT:
+               pygame.quit()  # close Window
+               quit()  # end pygame.
 
 
 
